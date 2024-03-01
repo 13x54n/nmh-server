2131126427cc584c41fd8e0c1c7faa6d43646968
13x54n <13x54r@gmail.com>
2024-03-01 08:10:08 -0500

    Added Socket.io

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+app.use(cors());
+
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
@@ -8,9 +14,6 @@ const userRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes");
 const productRouter = require("./routes/productRoutes");
 
-const app = express();
-
-app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
@@ -32,6 +35,30 @@ app.use("/api/v1/product", productRouter);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URI,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A client connected");
+
+  socket.on("message", (data) => {
+    console.log("Received message:", data);
+    // You can process the received message here and send a response back if needed
+
+    // Example: Sending a response back to the client
+    socket.emit("response", "Hello from the server!");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
